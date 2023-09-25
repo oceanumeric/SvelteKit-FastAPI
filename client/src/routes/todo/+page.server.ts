9f87b501c7738b43063f011c2fa70a00861b981e
oceanumeric <numerical.ocean@gmail.com>
2023-09-25 13:03:07 +0200

    [add] todo list

---
+++ client/src/routes/todo/+page.server.ts
@@ -0,0 +1,31 @@
+import { db } from '$lib/server/database';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+	// when the page is loaded, we need to fetch from the database
+	// and return the data to the page
+	const todos = await db.todo.findMany();
+
+	return { todos };
+};
+
+
+
+export const actions = {
+    // when the user do some actions like create, update, delete
+    addTodo: async({request}) => {
+        const todoFormData = await request.formData();
+        const todoItem = todoFormData.get('todo');
+        console.log(todoItem);
+
+        // save to database
+        const todo = await db.todo.create({
+            data: {
+                title: todoItem,
+                completed: false
+            }
+        });
+    
+    }
+    // delte todo
+}
