79f370b128aba51035d9687aade95180d4fb987b
oceanumeric <numerical.ocean@gmail.com>
2023-09-26 22:16:56 +0200

    [add] delete todos :)

--- client/src/routes/todo/+page.server.ts
+++ client/src/routes/todo/+page.server.ts
@@ -9,23 +9,34 @@ export const load: PageServerLoad = async () => {
 	return { todos };
 };
 
-
-
 export const actions = {
-    // when the user do some actions like create, update, delete
-    addTodo: async({request}) => {
-        const todoFormData = await request.formData();
-        const todoItem = todoFormData.get('todo');
-        console.log(todoItem);
+	// when the user do some actions like create, update, delete
+	addTodo: async ({ request }) => {
+		const todoFormData = await request.formData();
+		const todoItem = todoFormData.get('todo');
+		console.log(todoItem);
 
-        // save to database
-        const todo = await db.todo.create({
-            data: {
-                title: todoItem,
-                completed: false
+		// save to database
+		const todo = await db.todo.create({
+			data: {
+				title: todoItem,
+				completed: false
+			}
+		});
+	},
+	// delte todo
+    deleteTodo: async ({ request }) => {
+        const todoFormData = await request.formData();
+        const todoId = todoFormData.get('todoId');
+        console.log(todoId);
+        // convert to number
+        const todoIdNumber = parseInt(todoId as string);
+
+        // delete from database
+        const todo = await db.todo.delete({
+            where: {
+                id: todoIdNumber
             }
         });
-    
     }
-    // delte todo
-}
+};
--- client/src/routes/todo/+page.svelte
+++ client/src/routes/todo/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { enhance } from '$app/forms';
 
 	export let data: PageData;
 
-	console.log(data.todos);
+	// console.log("client side", data.todos);
 </script>
 
 <!-- component -->
@@ -11,45 +12,34 @@
 	<div class="bg-white rounded shadow p-6 m-4 w-full lg:w-5/6 lg:max-w-lg">
 		<div class="mb-4">
 			<h1 class="text-grey-darkest">Todo List</h1>
-			<div class="flex mt-4">
-				<form method="POST" action="?/addTodo">
+			<form method="POST" action="?/addTodo" use:enhance>
+				<div class="flex mb-4 items-center">
 					<input
-                        type="text"
-                        name="todo"
+						type="text"
+						name="todo"
+						autocomplete="off"
 						class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
 						placeholder="Add Todo"
 					/>
 					<button
-						class="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal"
+						class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red"
 						>Add</button
 					>
-				</form>
-			</div>
+				</div>
+			</form>
 		</div>
 		<div>
-			<div class="flex mb-4 items-center">
-				<p class="w-full text-grey-darkest">Add another component to Tailwind Components</p>
-				<button
-					class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green"
-					>Done</button
-				>
-				<button
-					class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red"
-					>Remove</button
-				>
-			</div>
-			<div class="flex mb-4 items-center">
-				<p class="w-full line-through text-green">
-					Submit Todo App Component to Tailwind Components
-				</p>
-				<button
-					class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-grey border-grey hover:bg-grey"
-					>Not Done</button
-				>
+			<div class="grid grid-cols-1">
+				{#each data.todos as todo, idx}
+				<p class="w-full text-grey-darkest">{idx+1}-{todo.title}</p>
+				<form method="POST" action="?/deleteTodo" use:enhance>
+				<input type="hidden" name="todoId" value="{todo.id}" />
 				<button
 					class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red"
 					>Remove</button
 				>
+				</form>
+				{/each}
 			</div>
 		</div>
 	</div>
